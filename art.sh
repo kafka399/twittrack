78e62629fc2a0a0b3d123054240e3b912dd0efe1
Dzidas <dm@datalab.lu>
2013-03-19 17:47:09 +0000

    twitter requires authorization to make a request

--- art.sh
+++ art.sh
@@ -1,3 +1,8 @@
+kelias=`pwd`
+cd /mnt/git/twitTracker/
 RMDFILE=report
 Rscript -e "require(knitr); knit('$RMDFILE.Rmd', '$RMDFILE.md');"
+cat $RMDFILE.md>>total.md
 pandoc -s -S -i $RMDFILE.md -o $RMDFILE.html 
+cat report.md
+cd $kelias
